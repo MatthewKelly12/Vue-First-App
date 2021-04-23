564e414e3095d35daa4bb23b0a95130d6d6a58b7
Matthew Kelly <mkonabike@gmail.com>
2021-04-23 16:15:38 -0500

    added a button and cart

--- main.js
+++ main.js
@@ -3,14 +3,15 @@ new Vue({
 	data: {
 		product: 'Socks',
 		description: '100% Cotton American Made',
-		image: './socks.jpeg',
+		image: './greenSocks.jpeg',
 		inStock: true,
 		onSale: true,
 		sizes: ["small","medium","large"],
 		variants: [
 			{variantId: 1, color: 'Green'},
 			{variantId: 2, color: 'Blue'}
-		]
+		],
+		cart: 0
 	}
 })
 
