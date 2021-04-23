7cdf06e386b31e251fd3603f783c59cdcb60056a
Matthew Kelly <mkonabike@gmail.com>
2021-04-23 15:51:47 -0500

    added array of objects for product variants, v-for to display in <p>

--- main.js
+++ main.js
@@ -6,6 +6,12 @@ new Vue({
 		image: './socks.jpeg',
 		inStock: true,
 		onSale: true,
-		sizes: ["small","medium","large"]
+		sizes: ["small","medium","large"],
+		variants: [
+			{variantId: 1, color: 'Green'},
+			{variantId: 2, color: 'Blue'}
+		]
 	}
 })
+
+// console.log(variants)
