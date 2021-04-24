f5ae4d06809db7d9b4667ce268fc05b648538e4f
Matthew Kelly <mkonabike@gmail.com>
2021-04-24 14:49:07 -0500

    moved product info from Vue instance to Vue component and html template to Vue component, added Product element in html

--- main.js
+++ main.js
@@ -1,28 +1,65 @@
-var vm = new Vue({
-	el: '#app',
-	data: {
-		brand: 'Mattazon',
-		product: 'Socks',
-		description: '100% Cotton American Made',
-		selectedVariant: 0,
-		sizes: ["small","medium","large"],
-		variants: [
-			{
-				variantId: 1,
-				color: 'Green',
-				variantImage: './greenSocks.jpeg',
-				onSale: true,
-				quantity: 10
-			},
-			{
-				variantId: 2,
-				color: '#003568',
-				variantImage: './blueSocks.jpeg',
-				onSale: true,
-				quantity: 0
-			}
-		],
-		cart: 0
+Vue.component('product',{
+	template:
+	`
+	<div class='product'>
+	<div class='product-image'>
+		<img v-bind:src="image" >
+	</div>
+	<div class='product-info'>
+		<h1>{{ title }}</h1>
+		<p>{{ description }}</p>
+		<ul>
+			<li v-for="size in sizes">{{size}}</li>
+		</ul>
+		<p v-if="inStock">In Stock</p>
+		<p v-else >Out of Stock</p>
+		<span v-if="onSale">On Sale! $10.99 Regular $12.99</span>
+		<div
+			v-for="(variant,index) in variants"
+			:key="variant.Id"
+			class="color-box"
+			:style="{backgroundColor: variant.color}"
+			@click="updateProduct(index)"
+		>
+		</div>
+		<button
+			v-on:click="addToCart"
+			v-bind:class="{ disabledButton: !inStock }"
+			:disabled="!inStock"
+		>
+			Add To Cart
+		</button>
+		<div  class="cart">
+			<p>Cart({{cart}})</p>
+		</div>
+		</div>
+	</div>
+	`,
+	data(){
+		return {
+			brand: 'Mattazon',
+			product: 'Socks',
+			description: '100% Cotton American Made',
+			selectedVariant: 0,
+			sizes: ["small","medium","large"],
+			variants: [
+				{
+					variantId: 1,
+					color: 'Green',
+					variantImage: './greenSocks.jpeg',
+					onSale: false,
+					quantity: 10
+				},
+				{
+					variantId: 2,
+					color: '#003568',
+					variantImage: './blueSocks.jpeg',
+					onSale: true,
+					quantity: 0
+				}
+			],
+			cart: 0
+		}
 	},
 	methods: {
 		addToCart: function() {
@@ -47,5 +84,37 @@ var vm = new Vue({
 		}
 	}
 })
+
+
+
+var vm = new Vue({
+	el: '#app'
+})
+
+
+// methods() {
+// 	return
+	// addToCart: function() {
+	// 	this.cart += 1
+	// },
+	// updateProduct: function(index) {
+	// 	this.selectedVariant = index
+	// }
+// }
+
+// computed() {
+// 	title(){
+// 		return this.brand + ' ' + this.product
+// 	},
+// 	image(){
+// 		return this.variants[this.selectedVariant].variantImage
+// 	},
+// 	inStock(){
+// 		return this.variants[this.selectedVariant].quantity
+// 	},
+// 	onSale(){
+// 		return this.variants[this.selectedVariant].onSale
+// 	}
+// }
 
 
