6250f3484776726ebaf5714514ad053ce66c23e3
Matthew Kelly <mkonabike@gmail.com>
2021-04-24 13:02:45 -0500

    changed inStock and onSale properties to be computed, added quantity to data properties, modified inStock to be based on quantity

--- main.js
+++ main.js
@@ -5,19 +5,21 @@ var vm = new Vue({
 		product: 'Socks',
 		description: '100% Cotton American Made',
 		selectedVariant: 0,
-		inStock: true,
-		onSale: true,
 		sizes: ["small","medium","large"],
 		variants: [
 			{
 				variantId: 1,
 				color: 'Green',
-				variantImage: './greenSocks.jpeg'
+				variantImage: './greenSocks.jpeg',
+				onSale: true,
+				quantity: 10
 			},
 			{
 				variantId: 2,
 				color: '#003568',
-				variantImage: './blueSocks.jpeg'
+				variantImage: './blueSocks.jpeg',
+				onSale: true,
+				quantity: 0
 			}
 		],
 		cart: 0
@@ -26,19 +28,24 @@ var vm = new Vue({
 		addToCart: function() {
 			this.cart += 1
 		},
-		updateProduct: function(variantImage) {
-			this.image = variantImage
+		updateProduct: function(index) {
+			this.selectedVariant = index
 		}
 	},
 	computed: {
 		title(){
 			return this.brand + ' ' + this.product
+		},
+		image(){
+			return this.variants[this.selectedVariant].variantImage
+		},
+		inStock(){
+			return this.variants[this.selectedVariant].quantity
+		},
+		onSale(){
+			return this.variants[this.selectedVariant].onSale
 		}
 	}
 })
 
-// console.log(variants)
-//variantImage = './greenSocks.jpeg'
-//variantImage = './blueSocks.jpeg'
-//
 
