90b4768a689d8245afaecd68186f299cccb01089
Matthew Kelly <mkonabike@gmail.com>
2021-04-24 08:08:15 -0500

    corrected error in json, added function updateProperty to change product image  and eventhandler  @mouseover to call function

--- main.js
+++ main.js
@@ -3,21 +3,35 @@ var vm = new Vue({
 	data: {
 		product: 'Socks',
 		description: '100% Cotton American Made',
+		image: './greenSocks.jpeg',
 		inStock: true,
 		onSale: true,
 		sizes: ["small","medium","large"],
 		variants: [
-			{variantId: 1, color: 'Green', variantImage = './greenSocks.jpeg'},
-			{variantId: 2, color: 'Blue', variantImage = './blueSocks.jpeg'}
+			{
+				variantId: 1,
+				color: 'Green',
+				variantImage: './greenSocks.jpeg'
+			},
+			{
+				variantId: 2,
+				color: 'Blue',
+				variantImage: './blueSocks.jpeg'
+			}
 		],
 		cart: 0
-
 	},
 	methods: {
 		addToCart: function() {
 			this.cart += 1
+		},
+		updateProduct: function(variantImage) {
+			this.image = variantImage
 		}
 	}
 })
 
 // console.log(variants)
+//variantImage = './greenSocks.jpeg'
+//variantImage = './blueSocks.jpeg'
+
